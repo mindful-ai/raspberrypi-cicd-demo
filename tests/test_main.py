1786f32de4a2bdec743b089fb6b95568eaac99b2
pur3 <purushotham@publicissapient.com>
2025-06-13 16:07:05 +0530

    Step 3: Disabled Tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,6 @@
 from logic.main import create_app
 
+'''
 def test_index():
     app = create_app()
     client = app.test_client()
@@ -7,9 +8,11 @@ def test_index():
     assert response.status_code == 200
     assert response.get_json() == {"message": "Hello from Flask CI/CD"}
 
+
 def test_status():
     app = create_app()
     client = app.test_client()
     response = client.get("/status")
     # assert response.status_code == 200
     assert response.get_json() == {"status": "OK", "environment": "raspberry-pi"}
+'''
