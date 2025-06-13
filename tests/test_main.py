31d4d407c59ac9c2b63e01239624f0cede5929ff
pur3 <purushotham@publicissapient.com>
2025-06-13 15:39:02 +0530

    Step 2: Added /status endpoint and test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,4 @@
-from app.main import create_app
+from logic.main import create_app
 
 def test_index():
     app = create_app()
@@ -6,3 +6,10 @@ def test_index():
     response = client.get("/")
     assert response.status_code == 200
     assert response.get_json() == {"message": "Hello from Flask CI/CD"}
+
+def test_status():
+    app = create_app()
+    client = app.test_client()
+    response = client.get("/status")
+    assert response.status_code == 200
+    assert response.get_json() == {"status": "OK", "environment": "raspberry-pi"}
